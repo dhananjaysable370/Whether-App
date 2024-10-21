7846b187a2ade49fc1b43f3070c62b4701fe6891
Dhananjay <dhananjaysable370@gmail.com>
2024-10-21 22:11:53 +0530

    Whether app

--- src/Components/SearchCity.jsx
+++ src/Components/SearchCity.jsx
@@ -1,14 +1,24 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import { useState } from 'react';
 function SearchCity() {
+    const [city, setCity] = useState("");
+    const handleChange = (e) => {
+        setCity(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(city);
+    }
   return (
-    <div className="flex my-3 justify-center items-center h-80 w-80 border border-teal-400 text-white">
-          <form >
+    <div className="flex my-3 justify-center items-center h-80 w-80 border border-teal-400  bg-zinc-800">
+          <form onSubmit={handleSubmit}>
               <div className='my-5 text-white'>
-                  <TextField id="standard-basic" label="Standard" variant="Search" className='' /><br/>
+                  <TextField label="Standard" onChange={handleChange} variant="standard" /><br/>
               </div>
               <div className='my-5 flex items-center justify-center'>
-                  <Button variant="contained">Contained</Button>
+                  <Button variant="contained" type='submit'>Contained</Button>
               </div>
       </form>
     </div>
