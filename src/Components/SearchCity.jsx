40532ebe32ca3cbc4803f78760cf295ca702ad71
Dhananjay <dhananjaysable370@gmail.com>
2024-10-23 20:34:20 +0530

    axios

--- src/Components/SearchCity.jsx
+++ src/Components/SearchCity.jsx
@@ -1,8 +1,16 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import InputLabel from '@mui/material/InputLabel';
 import { useState } from 'react';
+import axios from 'axios';
+
 function SearchCity() {
+
+  const API_URL = "https://api.openweathermap.org/data/3.0/onecall";
+  const API_KEY = "a2bd4bb81e517d2301aa82140a015d5f";
+
+  let getWhetherInfo = async () => {
+    const response = await axios.get(`${API_URL}`)
+  }
     const [city, setCity] = useState("");
     const handleChange = (e) => {
         setCity(e.target.value);
@@ -14,11 +22,11 @@ function SearchCity() {
   return (
     <div className="flex my-3 justify-center items-center h-80 w-80 border border-teal-400  bg-zinc-800">
           <form onSubmit={handleSubmit}>
-              <div className='my-5 text-white'>
-                  <TextField label="Standard" onChange={handleChange} variant="standard" /><br/>
+        <div className='my-5 text-white'>
+            <TextField id="filled-basic" className='text-white' label="Search city" variant="filled" onChange={handleChange} /><br />
               </div>
               <div className='my-5 flex items-center justify-center'>
-                  <Button variant="contained" type='submit'>Contained</Button>
+                  <Button variant="contained" type='submit'>Search</Button>
               </div>
       </form>
     </div>
